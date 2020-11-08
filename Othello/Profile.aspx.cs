85284b942f9d00e6d4c0530f18f9148f16f4c289
Luis Da Castellanos <luis.danniel@hotmail.com>
2020-11-07 22:07:43 -0600

    Perfil 100%

--- Othello/Profile.aspx.cs
+++ Othello/Profile.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -31,6 +32,7 @@ namespace Othello
 
         public void DesplegarData()
         {
+            DatosUsuario();
             PartidasGanadas();
             PartidasEmpatadas();
             PartidasPerdidas();
@@ -40,34 +42,160 @@ namespace Othello
         }
 
 
+        public void DatosUsuario()
+        {
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT nombre, apellido, correo, fecha_nac, pais FROM Usuario " +
+                      "WHERE username=\'" + user.Text + "\'", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Unombre.Text = registro.GetString(0);
+                    Uapellido.Text = registro.GetString(1);
+                    Ucorreo.Text = registro.GetString(2);
+                    Ufecha.Text = registro.GetDateTime(3).ToString().Replace("12:00:00 a. m.", "");
+                    Upais.Text = registro.GetString(4);
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                ClientScript.RegisterStartupScript(GetType(), "hwa", "alert(\"No se ha podido reconocer algún campo o bien, se encuentra vacío.\")", true);
+            }
+        }
+
         public void PartidasGanadas()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT COUNT(*) FROM Partida WHERE ganador = '" + user.Text + "'", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Pganadas.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Pganadas.Text = "0";
+            }
         }
 
         public void PartidasEmpatadas()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT COUNT(*) FROM Partida WHERE jugador1 = '" + user.Text + "' AND empate = 1", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Pempatadas.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Pempatadas.Text = "0";
+            }
         }
 
         public void PartidasPerdidas()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT COUNT(*) FROM Partida WHERE perdedor = '" + user.Text + "'", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Pperdidas.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Pperdidas.Text = "0";
+            }
         }
 
         public void TorneosJugados()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT COUNT(*) FROM Jugador WHERE usuario = '" + user.Text + "'", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Tjugados.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Tjugados.Text = "0";
+            }
         }
 
         public void TorneosGanados()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("select count(gan.ganador) as [Ganador], usar.usuario as [Usuario] from RondaTorneo gan inner join " +
+                    "Jugador usar on gan.ganador = usar.equipo where usar.usuario = '" + user.Text +"' AND gan.puntos = 3 AND(gan.ronda = 'Desempate Final' " +
+                    "OR gan.ronda = 'Final') group by usar.usuario", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Tganados.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Tganados.Text = "0";
+            }
         }
 
         public void PuntosTorneo()
         {
-
+            try
+            {//Codigo de AspNet Ya.com
+                string a = System.Configuration.ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString;
+                SqlConnection conexion = new SqlConnection(a);
+                conexion.Open();
+                SqlCommand script = new SqlCommand("SELECT sum(gan.puntos) as [Puntos], usar.usuario as [Usuario] " +
+                    "from RondaTorneo gan inner join Jugador usar on gan.equipo1 = usar.equipo OR gan.equipo2 = usar.equipo WHERE usar.usuario = '" + user.Text +
+                    "' GROUP BY usar.usuario", conexion);
+                SqlDataReader registro = script.ExecuteReader();
+                if (registro.Read())
+                {
+                    Tpuntos.Text = registro.GetValue(0).ToString();
+                }
+                conexion.Close();
+            }
+            catch (Exception)
+            {
+                Tpuntos.Text = "0";
+            }
         }
 
     }
--- Othello/Profile.aspx.designer.cs
+++ Othello/Profile.aspx.designer.cs
@@ -33,13 +33,13 @@ namespace Othello
         protected global::System.Web.UI.WebControls.LinkButton exit;
 
         /// <summary>
-        /// Control Label11.
+        /// Control lblInfo.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label11;
+        protected global::System.Web.UI.WebControls.Label lblInfo;
 
         /// <summary>
         /// Control user.
@@ -51,6 +51,33 @@ namespace Othello
         protected global::System.Web.UI.WebControls.Label user;
 
         /// <summary>
+        /// Control panelDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelDatos;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control Unombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Unombre;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
@@ -60,102 +87,174 @@ namespace Othello
         protected global::System.Web.UI.WebControls.Label Label1;
 
         /// <summary>
-        /// Control partidasGanadas.
+        /// Control Uapellido.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label partidasGanadas;
+        protected global::System.Web.UI.WebControls.Label Uapellido;
 
         /// <summary>
-        /// Control Label2.
+        /// Control Label11.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label2;
+        protected global::System.Web.UI.WebControls.Label Label11;
 
         /// <summary>
-        /// Control torneosParticipados.
+        /// Control Ucorreo.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label torneosParticipados;
+        protected global::System.Web.UI.WebControls.Label Ucorreo;
 
         /// <summary>
-        /// Control Label3.
+        /// Control Label13.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label3;
+        protected global::System.Web.UI.WebControls.Label Label13;
 
         /// <summary>
-        /// Control partidasEmpatadas.
+        /// Control Ufecha.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label partidasEmpatadas;
+        protected global::System.Web.UI.WebControls.Label Ufecha;
 
         /// <summary>
-        /// Control Label5.
+        /// Control Label15.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label5;
+        protected global::System.Web.UI.WebControls.Label Label15;
 
         /// <summary>
-        /// Control torneosGanados.
+        /// Control Upais.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label torneosGanados;
+        protected global::System.Web.UI.WebControls.Label Upais;
 
         /// <summary>
-        /// Control Label7.
+        /// Control Label8.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label7;
+        protected global::System.Web.UI.WebControls.Label Label8;
 
         /// <summary>
-        /// Control partidasPerdidas.
+        /// Control Pganadas.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label partidasPerdidas;
+        protected global::System.Web.UI.WebControls.Label Pganadas;
 
         /// <summary>
-        /// Control Label9.
+        /// Control Label16.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label9;
+        protected global::System.Web.UI.WebControls.Label Label16;
 
         /// <summary>
-        /// Control puntosTorneos.
+        /// Control Pempatadas.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label puntosTorneos;
+        protected global::System.Web.UI.WebControls.Label Pempatadas;
+
+        /// <summary>
+        /// Control Label18.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label18;
+
+        /// <summary>
+        /// Control Pperdidas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Pperdidas;
+
+        /// <summary>
+        /// Control Label20.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label20;
+
+        /// <summary>
+        /// Control Tjugados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Tjugados;
+
+        /// <summary>
+        /// Control Label22.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label22;
+
+        /// <summary>
+        /// Control Tganados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Tganados;
+
+        /// <summary>
+        /// Control Label24.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label24;
+
+        /// <summary>
+        /// Control Tpuntos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Tpuntos;
     }
 }
